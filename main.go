03e2b5617ec57b63ed1c8f01f143f77ade7098cc
Lucas-Melo0 <lucassss8v@gmail.com>
2022-11-04 16:11:36 -0300

    feat: Add adress status check to se if the adress is up

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+
+	addresses := []string{"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, adr := range addresses {
+		checkUrl(adr)
+	}
+}
+
+func checkUrl(url string) {
+	_, err := http.Get(url)
+	if err != nil {
+		fmt.Println(url, "might be down")
+		return
+	}
+
+	fmt.Println(url, "is up and running!")
 
 }
